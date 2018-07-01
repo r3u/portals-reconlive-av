5b6917e409ab1d9c90969cb276a11f9bdff7d8fd
Henry Rodrick <moshisushi@gmail.com>
2018-07-01 11:26:51 +0100

    License fix

--- app/app.py
+++ app/app.py
@@ -1,7 +1,8 @@
 #
 # pOrtals::reconLIVE:AV
 #
-# Copyright (C) 2018 by its
+# Copyright (C) 2018  Rachael Melanson
+# Copyright (C) 2018  Henry Rodrick
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
